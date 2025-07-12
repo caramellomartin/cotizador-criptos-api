a0a87b248b41b26ea11ecfb17cb30cb25fb5aacf
Caramello Martin <martincaramelloutn@gmail.com>
2025-07-13 01:50:01 +0200

    Completando estructura, agregando zod y completando css y componente CryptoSearchForm

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+import type { Currency } from "../types"
+
+export const currencies : Currency[] = [
+  { code: 'USD', name: 'Dolar de Estados Unidos'},
+  { code: 'EUR', name: 'Euro'},
+  { code: 'GBP', name: 'Libra Esterlina'},
+]
