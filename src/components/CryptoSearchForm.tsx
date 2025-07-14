c5b26cb98d575b74bd42adf38f9c26236d689adc
Caramello Martin <martincaramelloutn@gmail.com>
2025-07-14 13:21:06 +0200

    Manejo de API

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,11 +1,51 @@
+import { useCryptoStore } from "../store"
 import { currencies } from "../data"
+import { useState } from "react"
+import type { Pair } from "../types"
+import ErrorMessage from "./ErrorMessage"
 
 export default function CryptoSearchForm() {
+  const cryptoCurrencies = useCryptoStore((state) => state.cryptoCurrencies)
+  const fetchData = useCryptoStore((state) => state.fetchData)
+
+  const [ pair, setPair ] = useState<Pair>({
+    currency: '',
+    cryptoCurrency: ''
+  })
+
+  const [ error, setError ] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPair({
+      ...pair,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(Object.values(pair).includes('')) {
+      setError('Todos los campos son obligatorios.')
+      return
+    }
+    setError('')
+    fetchData(pair)
+  }
+
   return (
-    <form className="form">
+    <form 
+      className="form" 
+      onSubmit={handleSubmit}
+    >
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       <div className="field">
         <label htmlFor="currency">Moneda: </label>
-        <select name="currency" id="currency">
+        <select 
+          name="currency" 
+          id="currency"
+          value={pair.currency}
+          onChange={handleChange}
+        >
           <option value="">--Seleccione--</option>
           {currencies.map( currency => (
             <option key={currency.code} value={currency.code}>{currency.name}</option>
@@ -14,8 +54,19 @@ export default function CryptoSearchForm() {
       </div>
       <div className="field">
         <label htmlFor="cryptoCurrency">Criptomoneda: </label>
-        <select name="cryptoCurrency" id="cryptoCurrency">
+        <select 
+          name="cryptoCurrency" 
+          id="cryptoCurrency"
+          value={pair.cryptoCurrency}
+          onChange={handleChange}
+        >
           <option value="">--Seleccione--</option>
+          {cryptoCurrencies.map( crypto => (
+            <option
+              key={crypto.CoinInfo.FullName}
+              value={crypto.CoinInfo.Name}
+            >{crypto.CoinInfo.FullName}</option>
+          ))}
         </select>
       </div>
       <input type="submit" value='Cotizar' />
--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,29 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import type { CryptoCurrency } from "./types"
-import { getCryptos } from "./services/CryptoService"
+import type { CryptoCurrency, CryptoPrice, Pair } from "./types"
+import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService"
 
 type CryptoStore = {
   cryptoCurrencies: CryptoCurrency[]
+  result: CryptoPrice
   fetchCryptos: () => Promise<void>
+  fetchData: (pair: Pair) => Promise<void>
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools(
   (set) => ({
     cryptoCurrencies: [],
+    result: {} as CryptoPrice,
     fetchCryptos: async () => {
       const cryptoCurrencies = await getCryptos()
       set(() => ({
         cryptoCurrencies
+      }))
+    },
+    fetchData: async (pair) => {
+      const result = await fetchCurrentCryptoPrice(pair)
+      set(() => ({
+        result
       }))
     }
   }))
